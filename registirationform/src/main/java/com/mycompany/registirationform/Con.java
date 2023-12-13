5e9476239ca8e6c09e7cd0abad51f1d2cce18017
ishimwe266 <149780122+ishimwe266@users.noreply.github.com>
2023-12-13 12:10:24 +0200

    Add files via upload

---
+++ registirationform/src/main/java/com/mycompany/registirationform/Con.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.registirationform;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author USER
+ */
+class Con {
+
+    static PreparedStatement PreParedtatement(string query) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static PreparedStatement PreParedtatement(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ registirationform/src/main/java/com/mycompany/registirationform/Registirationform.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registirationform;
+
+/**
+ *
+ * @author USER
+ */
+public class Registirationform {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
